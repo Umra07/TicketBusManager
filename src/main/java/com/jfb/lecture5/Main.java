bb1baa3d96c4ed5d44ca4f5ba669e69659d36097
Volodymyr Karpenko <vol.karpenko07@gmail.com>
2024-10-21 02:12:56 +0200

    HW7: migrate from maven to gradle

--- src/main/java/com/jfb/lecture5/Main.java
+++ src/main/java/com/jfb/lecture5/Main.java
@@ -8,21 +8,23 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) throws JsonProcessingException {
-    int x = 0;
+    System.out.println("WORKS!");
 
-    do {
-      String input = getInput();
-      BusTicket busTicket = new ObjectMapper().readValue(input, BusTicket.class);
-
-      // TODO: ticket validation
-
-      System.out.println(busTicket.toString());
-      x++;
-
-    } while (x < 5);
+//    int x = 0;
+//
+//    do {
+//      String input = getInput();
+//      BusTicket busTicket = new ObjectMapper().readValue(input, BusTicket.class);
+//
+//      // TODO: ticket validation
+//
+//      System.out.println(busTicket.toString());
+//      x++;
+//
+//    } while (x < 5);
   }
 
-  private static String getInput() {
-    return new Scanner(System.in).nextLine();
-  }
+//  private static String getInput() {
+//    return new Scanner(System.in).nextLine();
+//  }
 }
